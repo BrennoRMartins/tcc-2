374eb3214e11a74ac658b457e2e24124bf67a690
BrennoRMartins <115500670+BrennoRMartins@users.noreply.github.com>
2025-02-05 12:46:39 -0300

    Add files via upload

---
+++ app/static/script.js
@@ -0,0 +1,58 @@
+let emoEscolhida = ''; 
+
+function changeButtonColor(color, text, emotion) {
+    var pronto = document.getElementById("pronto");
+    pronto.style.backgroundColor = color;
+    pronto.innerText = text;
+    pronto.style.color = "white";
+    
+    emoEscolhida = emotion; // define a emoção selecionada
+};
+
+function analise(emotion){
+    alert("Emoção Escolhida " + emotion);
+};
+
+// Adiciona evento de clique para cada emoção
+document.getElementById("alegria").addEventListener("click", function() {
+    changeButtonColor("#f0e002", "Pronto🥳", "alegria");
+});
+document.getElementById("medo").addEventListener("click", function() {
+    changeButtonColor("#9000ce", "Pronto😨", "medo");
+});
+document.getElementById("nojo").addEventListener("click", function() {
+    changeButtonColor("#00ce33", "Pronto😖", "nojo");
+});
+document.getElementById("raiva").addEventListener("click", function() {
+    changeButtonColor("#db0030", "Pronto😠", "raiva");
+});
+document.getElementById("surpresa").addEventListener("click", function() {
+    changeButtonColor("#e00069", "Pronto😲", "surpresa");
+});
+document.getElementById("tristeza").addEventListener("click", function() {
+    changeButtonColor("#007fe0", "Pronto☹️", "tristeza");
+});
+document.getElementById("amor").addEventListener("click", function() {
+    changeButtonColor("#d624b9", "Pronto🥰", "amor");
+});
+document.getElementById("curiosidade").addEventListener("click", function() {
+    changeButtonColor("#193db3", "Pronto🤯", "curiosidade");
+});
+
+// Redireciona para ranking.html com a emoção na URL ao clicar em "Pronto"
+document.getElementById("pronto").addEventListener("click", function(){
+    if (emoEscolhida) {
+        window.location.href = `ranking.html?emotion=${encodeURIComponent(emoEscolhida)}`;
+    } else {
+        alert("Por favor, selecione uma emoção antes de continuar.");
+    }
+});
+
+document.getElementById("avaliar").addEventListener("click", function(){
+    window.location.href = `avaliar.html`;
+});
+
+document.getElementById("submitBtn").addEventListener("click", function (e) {
+    window.location.href = `avaliar.html`;
+});
+
